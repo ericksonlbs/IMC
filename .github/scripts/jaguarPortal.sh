5496e691cfbdb0b5142a82a61b29dea30522c463
Erickson Lima <ericksonlbs@outlook.com>
2022-10-23 21:42:56 -0300

    add step jaguar submit

--- .github/scripts/jaguarPortal.sh
+++ .github/scripts/jaguarPortal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://fifilesync.azurewebsites.net/api/HttpTriggerHttpClient --header "X-API-Key: ${JAGUAR_PORTAL_APIKEY}"
+curl https://fifilesync.azurewebsites.net/api/HttpTriggerHttpClient --header "X-API-Key: ${JAGUARPORTAL_APIKEY}"
